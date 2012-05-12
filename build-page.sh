1ce7456facb367421e3e6c2d627a306f1a8612db
mattrude <m@mattrude.com>
2012-05-12 14:49:00 -0500

    Add back links to page

--- build-page.sh
+++ build-page.sh
@@ -3,6 +3,7 @@
 rm -f index.html README.md
 cat header > index.html && \
   git show master:README.md > README.md && \
+  sed -i '/# WordPress Webmaster Tools Plugin/a <p><a href="/">mattrude.github.com</a> / <strong>WordPress Webmaster Tools Plugin</strong></p>' README.md && \
   markdown README.md >> index.html && \
   cat footer >> index.html
 rm -f README.md
