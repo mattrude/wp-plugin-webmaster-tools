c2017cb9dc4e75b961e5d1b8125a4dcddd0b6be2
mattrude <m@mattrude.com>
2012-05-12 13:57:07 -0500

    First Commit

---
+++ build-page.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -f index.html README.md
+cat header > index.html && \
+  git show master:README.md > README.md && \
+  markdown README.md >> index.html && \
+  cat footer >> index.html
