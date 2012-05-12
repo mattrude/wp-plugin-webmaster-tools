a3362153e1106f8e1e2176437f4116ebcadda687
mattrude <m@mattrude.com>
2012-05-12 14:55:39 -0500

    Add back links to page

--- build-page.sh
+++ build-page.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+PAGENAME="WordPress Webmaster Tools Plugin"
+
 rm -f index.html README.md
 cat header > index.html && \
   git show master:README.md > README.md && \
-  sed -i '/# WordPress Webmaster Tools Plugin/c <div id="title"><h1>mattrude.github.com <i>&mdash; </i>WordPress Webmaster Tools Plugin</h1></div><p><a href="/">mattrude.github.com</a> / <strong>WordPress Webmaster Tools Plugin</strong></p>' README.md && \
+  sed -i "/# $PAGENAME/c <div id=\"title\"><h1>mattrude.github.com <i>&mdash; </i>$PAGENAME</h1></div><p><a href=\"/\">mattrude.github.com</a> / <strong>$PAGENAME</strong></p>" README.md && \
   markdown README.md >> index.html && \
   cat footer >> index.html
 rm -f README.md
